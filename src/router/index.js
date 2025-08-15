64c99f53f0dd7a886141a5c550fb77a5d4fb8565
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-08-15 13:56:56 +0200

    Create AddProject component Add to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import AddProject from '../views/AddProject.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: Home
   },
+  {
+    path: '/add',
+    name: 'AddProject',
+    component: AddProject
+  }
 ]
 
 const router = createRouter({
