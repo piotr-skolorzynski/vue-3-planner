b60e68bb277767779fa257e97d9262267c1fc65c
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-08-15 17:37:52 +0200

    Create EditProject component Create template Fetch particular project and display its data Add EditProject to router Modify SingleProject component to handle edit button

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -3,7 +3,9 @@
     <div class="actions" @click.self="toggleDetails">
       <h3>{{ project.title }}</h3>
       <div class="icons">
-        <span class="material-icons"> edit </span>
+        <router-link :to="{ name: 'EditProject', params: { id: project.id } }">
+          <span class="material-icons"> edit </span>
+        </router-link>
         <span class="material-icons" @click="deleteProject"> delete </span>
         <span
           class="material-icons"
---
+++ src/views/EditProject.vue
@@ -0,0 +1,35 @@
+<template>
+  <form>
+    <label for="title">Title:</label>
+    <input id="title" type="text" required v-model="title" />
+
+    <label for="details">Details:</label>
+    <textarea id="details" required v-model="details"></textarea>
+
+    <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      title: '',
+      details: '',
+      uri: `http://localhost:3000/projects/${this.id}`,
+    };
+  },
+  mounted() {
+    fetch(this.uri)
+      .then((response) => response.json())
+      .then((data) => {
+        this.title = data.title;
+        this.details = data.details;
+      })
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style></style>
