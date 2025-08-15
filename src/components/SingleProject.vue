0d29117985d0656cab88a5c933b584f57170915f
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-08-15 12:46:02 +0200

    Modify SingleProject component Add material icons

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -2,6 +2,11 @@
   <div class="project">
     <div class="actions" @click="toggleDetails">
       <h3>{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-icons"> edit </span>
+        <span class="material-icons"> delete </span>
+        <span class="material-icons"> done </span>
+      </div>
     </div>
     <div class="details" v-if="showDetails">
       <p>{{ project.details }}</p>
@@ -33,6 +38,23 @@ export default {
   border-radius: 4px;
   box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
   border-left: 4px solid #e90074;
+}
+
+.actions {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.material-icons {
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+
+.material-icons:hover {
+  color: #777;
 }
 
 h3 {
